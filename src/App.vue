2295a98d6cd4ae66ec1c75b451211aff74abe1ad
Vasile Cristel <v.cristel@mail.ru>
2020-04-05 13:00:18 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <Form @submitForm="onFormSubmit"/>
+    <TotalBalance :total="totalBalance"/>
+    <BudgetList :list="list" @deleteItem="onDeleteItem"/>
+  </div>
+</template>
+
+<script>
+import BudgetList from '@/components/BudgetList';
+import TotalBalance from '@/components/TotalBalance';
+import Form from '@/components/Form';
+
+export default {
+  name: 'App',
+  components: {
+    BudgetList,
+    TotalBalance,
+    Form
+  },
+  data: () => ({
+    list: {
+
+    }
+  }),
+  computed: {
+    totalBalance() {
+      return Object.values(this.list).reduce((acc, item) => acc + item.value, 0);
+    }
+  },
+  methods: {
+    onDeleteItem(id) {
+      this.$delete(this.list, id);
+    },
+    onFormSubmit(data) {
+      const newObj = {
+        ...data,
+        id: String(Math.random())
+      }
+
+      this.$set(this.list, newObj.id, newObj);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
